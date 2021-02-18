ec592582bc9f8b483db329b0de4b60001ba15540
jaryan77 <j.alexryan178@gmail.com>
2021-02-18 13:09:51 -0600

    table schema commit

---
+++ EmployeeSQL/table_schema.sql
@@ -0,0 +1,90 @@
+DROP TABLE dept_managers
+DROP TABLE dept_emp
+DROP TABLE departments
+DROP TABLE salaries
+DROP TABLE titles
+DROP TABLE employees
+
+CREATE TABLE departments (
+  dept_no VARCHAR(10) NOT NULL,
+  dept_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (dept_no)
+);
+
+select * from departments
+
+
+-- Create table employees
+
+
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR(20) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(20) NOT NULL,
+	last_name VARCHAR(20) NOT NULL,
+	gender VARCHAR(2) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+	PRIMARY KEY (emp_no)
+);
+
+select * from employees
+
+
+--Create table dept_emp
+
+
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+select * from dept_emp
+
+
+-- Create table dept_managers
+
+
+
+CREATE TABLE dept_managers (
+	dept_no VARCHAR (10) NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+select * from dept_managers
+
+
+
+--Creating salaries table
+
+
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+
+select * from salaries
+
+
+--Creating titles table
+
+
+
+CREATE TABLE titles (
+	title_id VARCHAR(20) NOT NULL,
+	title VARCHAR(20) NOT NULL,
+	PRIMARY KEY (title_id)
+);
+
+ n
