81e7775bf2f67c75e77be23b26afa0e6e08cff16
jaryan77 <j.alexryan178@gmail.com>
2021-02-18 14:11:36 -0600

    query #3 complete

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -14,4 +14,16 @@ FROM employees
 WHERE DATE_PART('year', hire_date) = 1986
 	ORDER BY hire_date;
 
---3.
+--3.List the manager of each department including: department number, department name, mgr's employee number, last name, first name
+SELECT dept_managers.emp_no,
+	dept_managers.dept_no,
+	departments.dept_name,
+	employees.first_name,
+	employees.last_name
+FROM dept_managers
+INNER JOIN departments ON
+dept_managers.dept_no = departments.dept_no
+INNER JOIN employees ON
+dept_managers.emp_no = employees.emp_no;
+
+
